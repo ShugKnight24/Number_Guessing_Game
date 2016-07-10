c3453a21d48b44f532552c5cb67c47405c51698c
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-10 00:31:13 -0400

    Cleaned up the JS, added another function.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,39 @@
 function guessNumber() {
-/* Use Math.random to generate a random number and Math.floor to round it down
-Necessary to multiply by 10 and add 1 becuase otherwise 0 would be included in the range
-Use parseInt because otherwise you would return a string and userNumber would never equal randomNumber*/
-  var randomNumber = Math.floor(Math.random() * 10) + 1;
-  var userNumber = parseInt(prompt("Enter a number between 1 and 10"));
 
-    if (randomNumber === userNumber) {
-      document.getElementById("result").innerText = "Good guess... Did you read my mind?";
-    } while (userNumber !== randomNumber) {
-        if (userNumber > randomNumber){
-        userNumber = parseInt(prompt("Nope! The number I'm thinking of is lower than what you guessed. Try again."));
-        } else if (userNumber < randomNumber){
-          userNumber = parseInt(prompt("Nope! The number I'm thinking of is higher than what you guessed. Try again."));
-        } else if (randomNumber === userNumber){
-          document.getElementById("result").innerText ="Good Work! Way to stick with it!";
-        } else {
-          userNumber = parseInt(prompt("You have enter a number between 1 and 10!"));
+/*** Clear the #result ID before running the function ***/
+  document.getElementById("result").innerText = "";
+
+/*** Generate a random number ***/
+  var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+/*** Can check the value of the random number in the console ***/
+  console.log(randomNumber)
+
+/*** User's guess ***/
+  var userNumber = getNumber("Please enter a number between 1 and 10");
+
+/*** If user guessed the random number on the first attempt ***/
+  if (userNumber === randomNumber) {
+      document.getElementById("result").innerText = "That was a lucky guess... You must have read my mind?";
     }
+/*** While Loop for if they are unable to get the right answer ***/
+        while (userNumber !== randomNumber){
+          if (userNumber > randomNumber){
+          userNumber = getNumber("Nope! The number I'm thinking of is lower than what you guessed. Try again.");
+          } else if (userNumber < randomNumber){
+            userNumber = getNumber("Nope! The number I'm thinking of is higher than what you guessed. Try again.");
+          }
   }
+/*** When user eventually gets to the final answer  ***/
+    document.getElementById("result").innerText ="Good Work! Way to stick with it!";
+}
+
+/*** A function within a function --- Gets a number from the user between 1 and 10 ***/
+function getNumber(message) {
+	var userNumber = parseInt(prompt(message));
+
+    while (!userNumber || userNumber < 1 || userNumber > 10) {
+    	userNumber = parseInt(prompt("Oops! You'll have to enter a number between 1 and 10 to get the program to work."));
+    }
+    return userNumber;
 }
